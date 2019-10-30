a89466a1323d11aa1591a66a16f105a3c3684b61
Glitch (third-breath) <none>
2019-10-30 18:22:14 +0000

    🔔🍍 Checkpoint ./routes/api.js:6684026/1446

--- routes/api.js
+++ routes/api.js
@@ -23,15 +23,24 @@ module.exports = function (app) {
       
       
       if(Array.isArray(stock)){
-        res.json(stock)
+        let stockObject = stock.map(x => {
+          let url = `/v1/stock/${x}/quote`;          
+          getStockInfo(baseUrl + url)
+          .then(result => {
+            return "this"
+          }, err => next(err))
+          .catch(err => next(err))
+        })
+        res.json(stockObject);
       }
       else if(stock && stock !== ''){
         let url = `/v1/stock/${stock}/quote`;
 
         getStockInfo(baseUrl + url)
-        .then(result => {res.json(result)
-                        })
-        .catch()
+        .then(result => {
+          res.json(result)
+        }, err => next(err))
+        .catch(err => next(err))
       }else{
         res.statusCode = 404;
         let err = new Error('No Stock Given');

febaf52773a26610445704d561d0b24c1f23172e
Glitch (third-breath) <none>
2019-10-30 16:12:37 +0000

    🏃🚏 Checkpoint ./routes/api.js:6684026/1727

--- routes/api.js
+++ routes/api.js
@@ -9,8 +9,33 @@ module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function (req, res, next){
-      // let stock = req.query.stock
-      // let like = req.query.like;
+      let baseUrl = 'https://repeated-alpaca.glitch.me';
+      let stock = req.query.stock
+      let like = req.query.like;
+      let stockData = {stockData: ''};
+      
+      if(Array.isArray(stock)){
+        res.json(stock)
+      }else{
+        if(like === 'true'){
+          res.json('like');
+        }else{
+          let url = `/v1/stock/${stock}/quote`
+          return fetch(baseUrl + url)
+          .then(response => {
+            if(response.ok){
+              return response
+            }else{
+              let err = new Error('Response returned not ok');
+              err.response = response;
+              throw err;
+            }
+          }, error => {
+            let err = new Error(error.message);
+            throw err;
+          })
+        }
+      }
       // if(!stock || stock === ''){
       //   res.statusCode = 404;
       //   let err = new Error('Stock not found');
@@ -18,7 +43,6 @@ module.exports = function (app) {
       // }else{
       //   res.json(stock);
       // }
-      res.json(req.ip)
     });
     
 };

442e1f7c613a256d605028e0aebe119c96a30487
Glitch (third-breath) <none>
2019-11-03 20:43:48 +0000

    🚁💽 Checkpoint ./views/index.html:6684026/1456 ./server.js:6684026/176

--- server.js
+++ server.js
@@ -16,31 +16,27 @@ app.use(helmet())
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(cors({origin: '*'}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Index page (static HTML)
 app.route('/')
   .get(function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
   });
 
-//For FCC testing purposes
+
 fccTestingRoutes(app);
 
-//Routing for API 
 apiRoutes(app);  
     
-//404 Not Found Middleware
 app.use(function(req, res, next) {
   res.status(404)
     .type('text')
     .send('Not Found');
 });
 
-//Start our server and tests!
 app.listen(process.env.PORT || 3000, function () {
   console.log("Listening on port " + process.env.PORT);
   if(process.env.NODE_ENV==='test') {
@@ -57,4 +53,4 @@ app.listen(process.env.PORT || 3000, function () {
   }
 });
 
-module.exports = app; //for testing
+module.exports = app; 

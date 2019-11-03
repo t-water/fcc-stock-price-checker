9282b6975875ef41f89b671f42f90e04161a7438
Glitch (third-breath) <none>
2019-11-03 22:13:47 +0000

    🎹📍 Checkpoint ./views/index.html:6684026/1048 ./routes/fcctesting.js:6684026/115 ./tests/2_functional-tests.js:6684026/35885

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,11 +1,3 @@
-/*
-*
-*
-*       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
-*       -----[Keep the tests in the same order!]-----
-*       (if additional are added, keep them at the very end!)
-*/
-
 var chaiHttp = require('chai-http');
 var chai = require('chai');
 var assert = chai.assert;
@@ -14,37 +6,88 @@ var server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-    
+  suite('Routing Tests', function() {
     suite('GET /api/stock-prices => stockData object', function() {
-      
       test('1 stock', function(done) {
        chai.request(server)
         .get('/api/stock-prices')
-        .query({stock: 'goog'})
+        .query({stock: 'fb'})
         .end(function(err, res){
-          
-          //complete this one too
-          
+          assert.equal(res.status, 200);
+          assert.property(res.body.stockData, 'stock');
+          assert.property(res.body.stockData, 'price');
+          assert.property(res.body.stockData, 'likes');
+          assert.equal(res.body.stockData.stock, 'FB');
           done();
         });
       });
       
+      var likes = '';
+      
       test('1 stock with like', function(done) {
-        
+       chai.request(server)
+        .get('/api/stock-prices')
+        .query({stock: 'fb', like: true})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.property(res.body.stockData, 'stock');
+          assert.property(res.body.stockData, 'price');
+          assert.property(res.body.stockData, 'likes');
+          assert.equal(res.body.stockData.stock, 'FB');
+          assert.isAbove(res.body.stockData.likes, 0);
+          likes = res.body.stockData.likes;
+          done();
+        });
       });
       
       test('1 stock with like again (ensure likes arent double counted)', function(done) {
-        
+       chai.request(server)
+        .get('/api/stock-prices')
+        .query({stock: 'fb', like: true})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.property(res.body.stockData, 'stock');
+          assert.property(res.body.stockData, 'price');
+          assert.property(res.body.stockData, 'likes');
+          assert.equal(res.body.stockData.stock, 'FB');
+          assert.equal(res.body.stockData.likes, likes);
+          done();
+        });
       });
-      
+            
       test('2 stocks', function(done) {
-        
+       chai.request(server)
+        .get('/api/stock-prices')
+        .query({stock: ['fb','goog']})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.property(res.body.stockData[0], 'stock');
+          assert.property(res.body.stockData[0], 'price');
+          assert.property(res.body.stockData[1], 'stock');
+          assert.property(res.body.stockData[1], 'price');
+          assert.oneOf(res.body.stockData[0].stock, ['FB','GOOG']);
+          assert.oneOf(res.body.stockData[1].stock, ['FB','GOOG']);
+          done();
+        });
       });
       
       test('2 stocks with like', function(done) {
-        
+       chai.request(server)
+        .get('/api/stock-prices')
+        .query({stock: ['fb','goog'], like: true})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.property(res.body.stockData[0], 'stock');
+          assert.property(res.body.stockData[0], 'price');
+          assert.property(res.body.stockData[0], 'likes');
+          assert.property(res.body.stockData[1], 'stock');
+          assert.property(res.body.stockData[1], 'price');
+          assert.property(res.body.stockData[1], 'likes');
+          assert.oneOf(res.body.stockData[0].stock, ['FB','GOOG']);
+          assert.oneOf(res.body.stockData[1].stock, ['FB','GOOG']);
+          done();
+        });
       });
-      
     });
-
+  });
 });
